8bb6dd98f1264ab55689fd25c30f2b26b5e58757
Anthony Ebube <egolumaer@gmail.com>
2022-08-07 12:11:12 +0100

    add new database connection

--- php/dbconn.php
+++ php/dbconn.php
@@ -1,10 +1,16 @@
 <?php
 //params to connection to a database
+//for develpment stage
+// $dbhost = "localhost";
+// $dbuser = "root";
+// $dbpass = "";
+// $dbname = "chat";
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "chat";
+//for production
+$dbhost = "remotemysql.com";
+$dbuser = "Um1yPsSz82";
+$dbpass = "FcA8jTWD4k";
+$dbname = "Um1yPsSz82";
 
 // connection to database
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
